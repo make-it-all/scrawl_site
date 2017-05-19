87dbe11b15adc63ce4e70b4c235282ccd860c657
Henry Morgan <henry@cd2solutions.co.uk>
2017-05-19 14:37:01 +0100

    HI

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,18 +3,18 @@ class NotesController < ApplicationController
   before_action :set_note, only: %i[show edit update]
 
   def index
-    @notes = Note.all
+    @notes = current_user.notes
   end
 
   def show
   end
 
   def new
-    @note = Note.new(user: current_user)
+    @note = current_user.notes.build(user: current_user)
   end
 
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.build(note_params)
     @note.user = current_user
     if @note.save
       redirect_to :notes, notice: 'Note Updated'
